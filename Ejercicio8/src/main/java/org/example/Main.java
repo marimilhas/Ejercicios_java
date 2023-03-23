d056a69aea5e88b5dd02187dc5da534fc23ecfd3
marimilhas <marimilhas@gmail.com>
2023-03-23 19:37:26 -0300

    Commit Nº3

---
+++ Ejercicio8/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese los siguientes datos de una persona: ");
+        System.out.println("Nombre: ");
+        String nombre = entrada.next();
+        System.out.println("Apellido: ");
+        String nombre = entrada.next();
+        System.out.println("Fecha de nacimiento: ");
+        String nombre = entrada.next();
+
+    }
+}
